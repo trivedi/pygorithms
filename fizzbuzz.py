653c2922409a3be4c3672cd019d58b5450d76406
Nishad Trivedi <nishadt15@gmail.com>
2013-11-25 01:55:34 -0600

    Added FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+""" The classic interview question """
+
+def FizzBuzz():
+    for i in range(1, 101):
+        if (i % 3 == 0 and i % 5 == 0): print "FizzBuzz"
+        elif (i % 3 == 0): print "Fizz"
+        elif (i % 5 == 0): print "Buzz"
+        else: print i
+
+
+if __name__ == '__main__':
+    FizzBuzz()
